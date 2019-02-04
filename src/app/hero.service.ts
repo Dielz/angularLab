bbba74f7a6169b86d53e2bd399c2ed1ab82fa130
Alfredo ramirez Samboy <Darkfalz@outlook.com>
2019-02-04 16:41:35 -0400

    packages update

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -14,7 +14,7 @@ const httpOptions = {
 @Injectable({ providedIn: 'root' })
 export class HeroService {
 
-  //private heroesUrl = 'api/heroes';  // URL to web api
+  // private heroesUrl = 'api/heroes';  // URL to web api
   private heroesUrl = 'http://localhost:10061/api/values';   // URL to web api ----
 
   constructor(
